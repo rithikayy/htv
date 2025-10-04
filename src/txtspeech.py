6887ff355aeab8cfbe658d7d151e69b565f98f02
rithikayy <rithika1226@gmail.com>
2025-10-04 17:45:37 -0400

    text to speech fixes, returning chunks instead of audio

--- src/txtspeech.py
+++ src/txtspeech.py
@@ -3,32 +3,49 @@ from elevenlabs import stream
 import os
 from dotenv import load_dotenv
 
+objects_said = set()
 
-def txttospeech(label, distance):
+def txttospeech(objects_to_be_said):
     load_dotenv()
-
+    chunk_lst = []
     elevenlabs_api = os.environ.get("xi-api-key")
 
     elevenlabs = ElevenLabs(api_key=elevenlabs_api)
+    
+    diff_check = False
 
-    if distance:
-        s = label + " " + str(distance) + "m away"
-        audio_stream = elevenlabs.text_to_speech.stream(
-            text=s,
-            voice_id="TVtDNgumMv4lb9zzFzA2", # normal voice!!!
-            model_id="eleven_multilingual_v2"
-        )
-    else:
-        audio_stream = elevenlabs.text_to_speech.stream(
-            text=label,
-            voice_id="TVtDNgumMv4lb9zzFzA2", # normal voice!!!
-            model_id="eleven_multilingual_v2"
-        )
+    for o in objects_to_be_said:
+        if o[0] not in objects_said:
+            diff_check = True
+            break
 
-    # option 1: play the streamed audio locally
-    stream(audio_stream)
+    if diff_check:
+        for o in objects_to_be_said:
+            if o[1]:
+                s = o[0] + " " + str(o[1]) + " meters away"
+                audio_stream = elevenlabs.text_to_speech.stream(
+                    text=s,
+                    voice_id="TVtDNgumMv4lb9zzFzA2", # normal voice!!!
+                    model_id="eleven_multilingual_v2"
+                )
+                for chunk in audio_stream:
+                    if isinstance(chunk, bytes):
+                        chunk_lst.append(chunk)
+            else:
+                audio_stream = elevenlabs.text_to_speech.stream(
+                    text=o[0],
+                    voice_id="TVtDNgumMv4lb9zzFzA2", # normal voice!!!
+                    model_id="eleven_multilingual_v2"
+                )
+                for chunk in audio_stream:
+                    if isinstance(chunk, bytes):
+                        chunk_lst.append(chunk)
 
-    # option 2: process the audio bytes manually
-    for chunk in audio_stream:
-        if isinstance(chunk, bytes):
-            print(chunk)
+    objects_said.clear()
+    for o in objects_to_be_said:
+        objects_said.add(o[0])
+
+    return chunk_lst
+
+        
+    # option 1: plays the streamed audio locally
